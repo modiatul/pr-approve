61d8e98e98f9bd75c19c3e38bc69cae458cd14a9
ajuner <106791576@qq.com>
2021-03-30 15:11:36 +0800

    feat: add approve params

--- src/main.js
+++ src/main.js
@@ -2,30 +2,25 @@ const core = require('@actions/core');
 const { Octokit } = require('@octokit/rest');
 const github = require('@actions/github');
 
+const token = core.getInput('token');
+const octokit = new Octokit({ auth: `token ${token}` });
+const context = github.context;
+
 async function run() {
   try {
-    const token = core.getInput('token');
-    const octokit = new Octokit({ auth: `token ${token}` });
-    const context = github.context;
-
     const { owner, repo } = context.repo;
+
     const number = context.payload.pull_request.number;
 
+    const comment = core.getInput('comment');
     await octokit.pulls.createReview({
       owner,
       repo,
       pull_number: number,
       event: 'APPROVE',
-      body: '1234',
+      body: comment,
     })
 
-    await octokit.pulls.merge({
-      owner,
-      repo,
-      pull_number: number,
-      commit_title: `Auto merge ${number}`,
-      commit_message: '123455'
-    })
   } catch (error) {
     core.setFailed(error.message);
   }
