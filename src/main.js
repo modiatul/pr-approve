d425ed02bfd1b6d44083c66792cf9c1c1976f9f8
ajuner <106791576@qq.com>
2021-03-30 16:57:31 +0800

    feat: add approve

--- src/main.js
+++ src/main.js
@@ -2,25 +2,24 @@ const core = require('@actions/core');
 const { Octokit } = require('@octokit/rest');
 const github = require('@actions/github');
 
-const token = core.getInput('token');
-const octokit = new Octokit({ auth: `token ${token}` });
-const context = github.context;
-
 async function run() {
   try {
-    const { owner, repo } = context.repo;
+    const token = core.getInput('token');
+    const octokit = new Octokit({ auth: `token ${token}` });
+    const context = github.context;
 
+    const { owner, repo } = context.repo;
     const number = context.payload.pull_request.number;
 
-    const comment = core.getInput('comment');
+    const body = core.getInput('comment');
+
     await octokit.pulls.createReview({
       owner,
       repo,
       pull_number: number,
       event: 'APPROVE',
-      body: comment,
-    })
-
+      body,
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
